cbf11b26c45ff2a1f91e5f0d820d5d15e50150f9
jinwhipark <knupjh@hanmail.net>
2019-08-22 10:33:33 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.bt_InputFile = new System.Windows.Forms.Button();
+            this.InputFileName = new System.Windows.Forms.TextBox();
+            this.pB_ShowPhoto = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_Result = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pB_ShowPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 108F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.bt_InputFile, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.InputFileName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pB_ShowPhoto, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tb_Result, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(537, 373);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // bt_InputFile
+            // 
+            this.bt_InputFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_InputFile.Location = new System.Drawing.Point(4, 4);
+            this.bt_InputFile.Name = "bt_InputFile";
+            this.bt_InputFile.Size = new System.Drawing.Size(102, 20);
+            this.bt_InputFile.TabIndex = 0;
+            this.bt_InputFile.Text = "그림 파일 입력";
+            this.bt_InputFile.UseVisualStyleBackColor = true;
+            this.bt_InputFile.Click += new System.EventHandler(this.bt_InputFile_Click);
+            // 
+            // InputFileName
+            // 
+            this.InputFileName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InputFileName.Location = new System.Drawing.Point(113, 4);
+            this.InputFileName.Name = "InputFileName";
+            this.InputFileName.Size = new System.Drawing.Size(420, 21);
+            this.InputFileName.TabIndex = 1;
+            // 
+            // pB_ShowPhoto
+            // 
+            this.pB_ShowPhoto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pB_ShowPhoto.Location = new System.Drawing.Point(113, 31);
+            this.pB_ShowPhoto.Name = "pB_ShowPhoto";
+            this.pB_ShowPhoto.Size = new System.Drawing.Size(420, 311);
+            this.pB_ShowPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pB_ShowPhoto.TabIndex = 2;
+            this.pB_ShowPhoto.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(4, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 311);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "입력 그림";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(4, 349);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "분류 결과";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb_Result
+            // 
+            this.tb_Result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_Result.Location = new System.Drawing.Point(113, 349);
+            this.tb_Result.Name = "tb_Result";
+            this.tb_Result.Size = new System.Drawing.Size(420, 21);
+            this.tb_Result.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 373);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "TensorFlowSharp Test";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pB_ShowPhoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button bt_InputFile;
+        private System.Windows.Forms.TextBox InputFileName;
+        private System.Windows.Forms.PictureBox pB_ShowPhoto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_Result;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TensorFlow;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Select Photo to inference
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bt_InputFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Images (*.JPG)|*.JPG|" + "All files (*.*)|*.*";
+            openFileDialog1.Title = "Select Photo";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                InputFileName.Text = openFileDialog1.FileName;
+                ShowInputPhoto(InputFileName.Text);
+                tb_Result.Text = Inference(InputFileName.Text);
+            }
+        }
+
+        /// <summary>
+        /// Show photo on Picturebox
+        /// </summary>
+        /// <param name="filename"></param>
+        private void ShowInputPhoto(string filename)
+        {
+            Bitmap myBitmap = new Bitmap(filename);
+            pB_ShowPhoto.Image = myBitmap;
+        }
+
+        static string dir, modelFile, labelsFile;
+        /// <summary>
+        /// Inference using TensorFlow model
+        /// </summary>
+        /// <param name="filename"></param>
+        private string Inference(string filename)
+        {
+            ModelFiles(@"C:\Users\jinwh\source\repos\WindowsFormsApp1\WindowsFormsApp1\model");
+
+            // Construct an in-memory graph from the serialized form.
+            var graph = new TFGraph();
+            // Load the serialized GraphDef from a file.
+            var model = File.ReadAllBytes(modelFile);
+
+            graph.Import(model, "");
+
+            using (var session = new TFSession(graph))
+            {
+                var labels = File.ReadAllLines(labelsFile);
+                
+                // Run inference on the image files
+                // For multiple images, session.Run() can be called in a loop (and
+                // concurrently). Alternatively, images can be batched since the model
+                // accepts batches of image data as input.
+                var tensor = CreateTensorFromImageFile(filename);
+
+                var runner = session.GetRunner();
+
+
+                //runner.AddInput(graph["input"][0], tensor).Fetch(graph["output"][0]);
+                runner.AddInput(graph["input"][0], tensor).Fetch(graph["InceptionV1/Logits/Predictions/Softmax"][0]);
+                var output = runner.Run();
+                // output[0].Value() is a vector containing probabilities of
+                // labels for each image in the "batch". The batch size was 1.
+                // Find the most probably label index.
+
+                var result = output[0];
+                var rshape = result.Shape;
+                if (result.NumDims != 2 || rshape[0] != 1)
+                {
+                    var shape = "";
+                    foreach (var d in rshape)
+                    {
+                        shape += $"{d} ";
+                    }
+                    shape = shape.Trim();
+                    Console.WriteLine($"Error: expected to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape [{shape}]");
+                    Environment.Exit(1);
+                }
+
+                // You can get the data in two ways, as a multi-dimensional array, or arrays of arrays, 
+                // code can be nicer to read with one or the other, pick it based on how you want to process
+                // it
+                bool jagged = true;
+
+                var bestIdx = 0;
+                float p = 0, best = 0;
+
+                if (jagged)
+                {
+                    var probabilities = ((float[][])result.GetValue(jagged: true))[0];
+                    for (int i = 0; i < probabilities.Length; i++)
+                    {
+                        if (probabilities[i] > best)
+                        {
+                            bestIdx = i;
+                            best = probabilities[i];
+                        }
+                    }
+
+                }
+                else
+                {
+                    var val = (float[,])result.GetValue(jagged: false);
+
+                    // Result is [1,N], flatten array
+                    for (int i = 0; i < val.GetLength(1); i++)
+                    {
+                        if (val[0, i] > best)
+                        {
+                            bestIdx = i;
+                            best = val[0, i];
+                        }
+                    }
+                }
+
+                return  $" best match: [{bestIdx}] {best * 100.0}% {labels[bestIdx]}";
+                
+            }
+
+            
+        }
+
+        static void ModelFiles(string dir)
+        {
+            string url = "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip";
+
+            //modelFile = Path.Combine(dir, "tensorflow_inception_graph.pb");
+            modelFile = Path.Combine(dir, "Tensorflow_inception_v1_caltech256_graph.pb");
+            //labelsFile = Path.Combine(dir, "imagenet_comp_graph_label_strings.txt");
+            labelsFile = Path.Combine(dir, "caltech256_comp_graph_label_strings.txt");
+            var zipfile = Path.Combine(dir, "inception5h.zip");
+
+            if (File.Exists(modelFile) && File.Exists(labelsFile))
+                return;
+
+            Directory.CreateDirectory(dir);
+            var wc = new WebClient();
+            wc.DownloadFile(url, zipfile);
+            ZipFile.ExtractToDirectory(zipfile, dir);
+            File.Delete(zipfile);
+        }
+
+        // Convert the image in filename to a Tensor suitable as input to the Inception model.
+        public static TFTensor CreateTensorFromImageFile(string file, TFDataType destinationDataType = TFDataType.Float)
+        {
+            var contents = File.ReadAllBytes(file);
+
+            // DecodeJpeg uses a scalar String-valued tensor as input.
+            var tensor = TFTensor.CreateString(contents);
+
+            TFOutput input, output;
+
+            // Construct a graph to normalize the image
+            using (var graph = ConstructGraphToNormalizeImage(out input, out output, destinationDataType))
+            {
+                // Execute that graph to normalize this one image
+                using (var session = new TFSession(graph))
+                {
+                    var normalized = session.Run(
+                        inputs: new[] { input },
+                        inputValues: new[] { tensor },
+                        outputs: new[] { output });
+
+                    return normalized[0];
+                }
+            }
+        }
+
+        // The inception model takes as input the image described by a Tensor in a very
+        // specific normalized format (a particular image size, shape of the input tensor,
+        // normalized pixel values etc.).
+        //
+        // This function constructs a graph of TensorFlow operations which takes as
+        // input a JPEG-encoded string and returns a tensor suitable as input to the
+        // inception model.
+        private static TFGraph ConstructGraphToNormalizeImage(out TFOutput input, out TFOutput output, TFDataType destinationDataType = TFDataType.Float)
+        {
+            // Some constants specific to the pre-trained model at:
+            // https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+            //
+            // - The model was trained after with images scaled to 224x224 pixels.
+            // - The colors, represented as R, G, B in 1-byte each were converted to
+            //   float using (value - Mean)/Scale.
+
+            const int W = 224;
+            const int H = 224;
+            const float Mean = 0.5f;            
+            const float Scale = 2.0f;
+                        
+            var graph = new TFGraph();
+            input = graph.Placeholder(TFDataType.String);            
+
+            var scale = graph.Div(
+                    x: graph.Const(1.0f),
+                    y: graph.Const(255.0f)
+                    );
+            
+            output = graph.Mul(
+                x: graph.Sub(
+                    x: graph.Squeeze(
+                        input: graph.ResizeBilinear(
+                            images: graph.ExpandDims(
+                                input: graph.Mul(
+                                        x: graph.Cast(graph.DecodeJpeg(contents: input, channels: 3), DstT: TFDataType.Float),
+                                        y: scale),
+                                dim: graph.Const(0, "make_batch")),
+                            size: graph.Const(new int[] { W, H }, "size"), 
+                            align_corners:false),
+                        squeeze_dims: new long[] { 0 }),
+                    y: graph.Const(Mean, "mean")),
+                y: graph.Const(Scale, "scale"));
+
+            output = graph.ExpandDims(
+                input: output,
+                dim: graph.Const(0));
+
+
+            //output = graph.Cast(graph.Div(
+            //    x: graph.Sub(
+            //        x: graph.ResizeBilinear(
+            //            images: graph.ExpandDims(
+            //                input: graph.Cast(
+            //                    graph.DecodeJpeg(contents: input, channels: 3), DstT: TFDataType.Float),
+            //                dim: graph.Const(0, "make_batch")),
+            //            size: graph.Const(new int[] { W, H }, "size")),
+            //        y: graph.Const(Mean, "mean")),
+            //    y: graph.Const(Scale, "scale")), destinationDataType);
+
+
+            //output = graph.Mul(
+            //    x: graph.Sub(
+            //        x: graph.Mul(
+            //            x: graph.Cast( 
+            //                graph.DecodeJpeg(contents: input, channels: 3), DstT: TFDataType.Float),
+            //            y: scale),
+            //        y: graph.Const(Mean, "mean")),
+            //    y: graph.Const(Scale, "scale"));
+
+
+
+            return graph;
+        }
+
+    }
+}
